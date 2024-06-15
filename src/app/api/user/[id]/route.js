9e92d1ac4bd5c1f12897f8f22b2e810ffa2eb905
nixon <jeromenixon95@gmail.com>
2024-06-15 13:15:43 -0700

    created api route for delete user and update user

--- src/app/api/user/[id]/route.js
+++ src/app/api/user/[id]/route.js
@@ -21,3 +21,46 @@ export async function GET(request, { params }) {
     return NextResponse.json({ error: 'Internal Server Error in route' }, { status: 500 });
   }
 }
+
+export async function DELETE(request, { params }) {
+  const { id } = params;
+
+  try {
+    await dbConnect();
+    const idObject = new ObjectId(id);
+    const user = await User.findById(idObject);
+
+    if (!user) {
+      return NextResponse.json({ error: 'trip not found' }, { status: 404 });
+    }
+    await User.findByIdAndDelete(idObject);
+    // await User.updateOne({ trips: idObject }, { $pull: { trips: idObject } });
+    return NextResponse.json({ message: 'User deleted successfully' });
+  } catch (error) {
+    console.error('Error fetching trips:', error);
+    return NextResponse.json({ error: 'Internal Server Error in route' }, { status: 500 });
+  }
+}
+
+export async function PUT(request, { params }) {
+  const { id } = params;
+  const res = await request.json();
+  const likedTripIds = res.likedTrips;
+  console.log('liked trips:', likedTripIds);
+  try {
+    await dbConnect();
+    const idObject = new ObjectId(id);
+    const user = await User.findById(idObject);
+
+    if (!user) {
+      return NextResponse.json({ error: 'user not found' }, { status: 404 });
+    }
+    const likedTripObjectIds = likedTripIds.map((tripId) => new ObjectId(tripId));
+    const update = { ...res, likedTrips: likedTripObjectIds };
+    const updatedUser = await User.findByIdAndUpdate(idObject, update, { new: true });
+    return NextResponse.json(updatedUser);
+  } catch (error) {
+    console.error('Error fetching trips:', error);
+    return NextResponse.json({ error: 'Internal Server Error in route' }, { status: 500 });
+  }
+}
